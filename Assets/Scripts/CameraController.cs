d513a473f2be2a979389b73305abce19938c2edc
prichiSupinfo <264144@supinfo.com>
2019-11-24 06:55:50 +0400

    Black Hole Added

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,10 +17,14 @@ public class CameraController : MonoBehaviour
 
     private void LateUpdate()
     {
-        var desiredPosition = target.position + offset;
-        var smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        if (target)
+        {
 
+            var desiredPosition = target.position + offset;
+            var smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+
+        }
         //transform.LookAt(target);
     }
 
